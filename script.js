1c3608a08483c778d36ff7d44a0e9acece22fcce
stefanipaulo <143711962+stefanipaulo@users.noreply.github.com>
2024-04-15 11:56:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function verificarIdade(idade) {
+    if (idade >= 18) {
+        console.log("Você é maior de idade.");
+    } else {
+        console.log("Você é menor de idade.");
+    }
+}
+
+// Exemplo de uso da função
+verificarIdade(20); // Saída: Você é maior de idade.
+verificarIdade(15); // Saída: Você é menor de idade.
